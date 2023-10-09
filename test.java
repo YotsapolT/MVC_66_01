1d1b15ea0f0a3d3a2b8b47e367354e1f17a81cfe
Tong <102939886+YotsapolT@users.noreply.github.com>
2023-10-09 22:09:36 +0700

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class test {
-    public static void main(String[] args) {
-        String a = "aaaaa//asdasdad";
-        a = a.substring(0, a.indexOf("//"));
-        System.out.println(a);
-    }
-}
